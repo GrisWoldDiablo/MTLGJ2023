bb4e5510632a2c159eb5b8b67f8844f4dbef7be5
Alonso <ugoalonso@hotmail.com>
2023-04-29 13:21:58 -0400

    Added acceleration when going back and forth

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -5,199 +5,225 @@ using UnityEngine;
 
 public enum PlayerState
 {
-	Jump,
-	Run,
-	Slide
+    Jump,
+    Run,
+    Slide
 }
 
 public class PlayerMovement : MonoBehaviour
 {
-	private Rigidbody2D _body;
-	[Header("Movement")] [SerializeField] private float _speed = 7f;
-	[SerializeField] private float _jumpHeight = 14f;
-	[SerializeField] private float _timeOfSlowdown = 3f;
-	[SerializeField] private float _slidingDuration = 2f;
-	[Header("Camera")] [SerializeField] private CameraMovement _camera;
+    private Rigidbody2D _body;
+    [Header("Movement")] [SerializeField] private float _speed = 7f;
+    [SerializeField] private float _jumpHeight = 14f;
+    [SerializeField] private float _timeOfSlowdown = 3f;
+    [SerializeField] private float _slidingDuration = 2f;
+    [Header("Camera")] [SerializeField] private CameraMovement _camera;
 
-	[Header("Collisions")] [SerializeField]
-	private ContactFilter2D _contactFilter2D;
-	private BoxCollider2D _boxCollider2D;
-	private Vector2 _standingColliderSize;
-	private Vector2 _standingColliderOffset;
-	private Vector2 _slidingColliderSize;
-	private Vector2 _slidingColliderOffset;
+    [Header("Collisions")] [SerializeField]
+    private ContactFilter2D _contactFilter2D;
+    private BoxCollider2D _boxCollider2D;
+    private Vector2 _standingColliderSize;
+    private Vector2 _standingColliderOffset;
+    private Vector2 _slidingColliderSize;
+    private Vector2 _slidingColliderOffset;
 	
-	private bool _isGrounded => _body.IsTouching(_contactFilter2D);
-	private bool _isSliding = false;
-	public bool IsGrounded => _isGrounded;
-	public bool IsMovingForward => _dirX > 0.0f;
-	public Vector2 Velocity => _body.velocity;
+    private bool _isGrounded => _body.IsTouching(_contactFilter2D);
+    private bool _isSliding = false;
+    private bool _isHit = false;
+    public bool IsGrounded => _isGrounded;
+    public bool IsMovingForward => _forwardSpeed > 0.0f;
+    public Vector2 Velocity => _body.velocity;
 
-	public bool IsDead { get; set; } = false; // TODO Move dead logic to Player Class
+    public bool IsDead { get; set; } = false; // TODO Move dead logic to Player Class
 
-	public PlayerState GetPlayerState()
-	{
-		if (!IsGrounded)
-		{
-			return PlayerState.Jump;
-		}
+    public PlayerState GetPlayerState()
+    {
+        if (!IsGrounded)
+        {
+            return PlayerState.Jump;
+        }
 
-		if (_isSliding)
-		{
-			return PlayerState.Slide;
-		}
+        if (_isSliding)
+        {
+            return PlayerState.Slide;
+        }
 
-		return PlayerState.Run;
-	}
+        return PlayerState.Run;
+    }
 
-	public float Speed
-	{
-		get => _speed;
-		set => _speed = value;
-	}
+    public float Speed
+    {
+        get => _speed;
+        set => _speed = value;
+    }
 
-	void Start()
-	{
-		_body = GetComponent<Rigidbody2D>();
-		_boxCollider2D = GetComponent<BoxCollider2D>();
-		_contactFilter2D.useNormalAngle = true;
-		_contactFilter2D.minNormalAngle = 89;
-		_contactFilter2D.maxNormalAngle = 91;
-		_standingColliderSize = _boxCollider2D.size;
-		_standingColliderOffset = _boxCollider2D.offset;
-		_slidingColliderSize = new Vector2(_standingColliderSize.x,_standingColliderSize.y / 2);
-		_slidingColliderOffset = new Vector2(_boxCollider2D.offset.x,_boxCollider2D.offset.y * 2);
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _contactFilter2D.useNormalAngle = true;
+        _contactFilter2D.minNormalAngle = 89;
+        _contactFilter2D.maxNormalAngle = 91;
+        _standingColliderSize = _boxCollider2D.size;
+        _standingColliderOffset = _boxCollider2D.offset;
+        _slidingColliderSize = new Vector2(_standingColliderSize.x,_standingColliderSize.y / 2);
+        _slidingColliderOffset = new Vector2(_boxCollider2D.offset.x,_boxCollider2D.offset.y * 2);
 
-	}
+    }
 
-	//Speed and time variables
-	private float _forwardSpeed = 1.1f;
-	private float _maxForwardSpeed = 1.1f;
-	private float _time = 0;
-	private float _slidingTime = 0;
-	private float _dirX;
-	private bool _isHittingRoof;
-	private bool _isSlidingUnderRoof = false;
+    //Speed and time variables
+    private float _forwardSpeed = 1.1f;
+    private float _maxForwardSpeed = 1.1f;
+    private float _time = 0;
+    private float _slidingTime = 0;
+    private float _dirX;
+    private bool _isHittingRoof;
+    private bool _isSlidingUnderRoof = false;
 
-	void Update()
-	{
-		_isHittingRoof = IsHittingRoof();
-		if (IsDead) // TODO Move dead logic to Player Class
-		{
-			_body.velocity = Vector2.zero;
-			return;
-		}
+    void Update()
+    {
+        _isHittingRoof = IsHittingRoof();
+        if (IsDead) // TODO Move dead logic to Player Class
+        {
+            _body.velocity = Vector2.zero;
+            return;
+        }
 
-		MoveLeftRight();
-		MoveUpDown();
-	}
+        MoveLeftRight();
+        MoveUpDown();
+    }
 
-	private void MoveLeftRight()
-	{
-		if (_isSlidingUnderRoof)
-		{
-			return;
-		}
+    private void MoveLeftRight()
+    {
+        if (_isSlidingUnderRoof)
+        {
+            return;
+        }
+
 		
-		_dirX = Input.GetAxisRaw("Horizontal");
-		if (_dirX < 0f)
-		{
-			_dirX = -0.3f;
-			_camera.AllowCameraX(false);
-			_isSliding = false;
+        _dirX = Input.GetAxisRaw("Horizontal");
+        if (_dirX < 0f)
+        {
+            if (_forwardSpeed > -0.3f)
+            {
+                _forwardSpeed += Time.deltaTime * -5f;
+            }
+            else
+            {
+                _forwardSpeed = -0.3f;
+            }
+            if (_forwardSpeed < 0)
+            {
+                _camera.AllowCameraX(false);
+            }
+            _isSliding = false;
 
-		}
-		else if (_dirX >= 0f)
-		{
-			if (_forwardSpeed < _maxForwardSpeed)
-			{
-				_time += Time.deltaTime;
-				if (_time > _timeOfSlowdown)
-				{
-					_forwardSpeed += Time.deltaTime * _maxForwardSpeed;
-				}
-			}
-			else
-			{
-				_time = 0;
-				_forwardSpeed = _maxForwardSpeed;
-			}
-			_dirX = _forwardSpeed;
-			_camera.AllowCameraX(true);
-		}
-		_body.velocity = new Vector2(_dirX * _speed, _body.velocity.y);
+        }
+        else
+        {
+            if (!_isHit)
+            {
+                if (_forwardSpeed < _maxForwardSpeed)
+                {
+                    _forwardSpeed += Time.deltaTime * _maxForwardSpeed * 5;
+                }
+                else
+                {
+                    _forwardSpeed = _maxForwardSpeed;
+                }
+
+                _camera.AllowCameraX(true);
+            }
+            else
+            {
+                _forwardSpeed = 0.5f;
+                _time += Time.deltaTime;
+                if (_time > _timeOfSlowdown)
+                {
+                    _isHit = false;
+                    _time = 0;
+                }
+                _camera.AllowCameraX(true);
+            }
+        }
 		
-		if (!_isSliding)
-		{
-			ResetCollisions();
-		}
-	}
+		
+        _body.velocity = new Vector2(_forwardSpeed * _speed, _body.velocity.y);
+        if (!_isSliding)
+        {
+            ResetCollisions();
+        }
+    }
 
 
-	private void MoveUpDown()
-	{
+    private void MoveUpDown()
+    {
 
-		if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")) && IsGrounded && !_isSlidingUnderRoof)
-		{
-			_body.velocity = new Vector2(_body.velocity.x, _jumpHeight);
-			_isSliding = false;
-		}
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")) && IsGrounded && !_isSlidingUnderRoof)
+        {
+            _body.velocity = new Vector2(_body.velocity.x, _jumpHeight);
+            _isSliding = false;
+        }
 
-		if (Input.GetButtonDown("Slide") && !_isSliding && IsGrounded && _dirX > 0)
-		{
-			_isSliding = true;
-			_slidingTime = _slidingDuration;
-			_boxCollider2D.size = _slidingColliderSize;
-			_boxCollider2D.offset = _slidingColliderOffset;
+        if (Input.GetButtonDown("Slide") && !_isSliding && IsGrounded && _forwardSpeed > 0)
+        {
+            _isSliding = true;
+            _slidingTime = _slidingDuration;
+            _boxCollider2D.size = _slidingColliderSize;
+            _boxCollider2D.offset = _slidingColliderOffset;
 
-		}
+        }
 
-		if (_isSliding)
-		{
-			if (_isHittingRoof)
-			{
-				_slidingTime -= Time.deltaTime;
-				_isSlidingUnderRoof = true;
-				return;
-			}
+        if (_isSliding)
+        {
+            if (_isHittingRoof)
+            {
+                _slidingTime -= Time.deltaTime;
+                _isSlidingUnderRoof = true;
+                return;
+            }
 			
-			if (_slidingTime > 0 )
-			{
-				_slidingTime -= Time.deltaTime;
-				_isSlidingUnderRoof = false;
-			}
-			else
-			{
-				_isSliding = false;
-				_isSlidingUnderRoof = false;
-			}
-		}
+            if (_slidingTime > 0 )
+            {
+                _slidingTime -= Time.deltaTime;
+                _isSlidingUnderRoof = false;
+            }
+            else
+            {
+                _isSliding = false;
+                _isSlidingUnderRoof = false;
+            }
+        }
 
-	}
-	private bool IsHittingRoof()
-	{
-		var hitPos1 = new Vector2(transform.position.x-0.1f,transform.position.y- 0.7f);
-		var hitPos2 = new Vector2(transform.position.x+0.1f,transform.position.y- 0.7f);
-		Debug.DrawLine(hitPos1,hitPos1+ Vector2.up* 0.5f,Color.red);
-		Debug.DrawLine(hitPos2,hitPos2+ Vector2.up* 0.5f,Color.red);
+    }
+    private bool IsHittingRoof()
+    {
+        var hitPos1 = new Vector2(transform.position.x-0.1f,transform.position.y- 0.7f);
+        var hitPos2 = new Vector2(transform.position.x+0.1f,transform.position.y- 0.7f);
+        Debug.DrawLine(hitPos1,hitPos1+ Vector2.up* 0.5f,Color.red);
+        Debug.DrawLine(hitPos2,hitPos2+ Vector2.up* 0.5f,Color.red);
 
-		RaycastHit2D hit = Physics2D.Raycast(hitPos1, Vector2.up,0.5f);
-		RaycastHit2D hit2 = Physics2D.Raycast(hitPos2, Vector2.up,0.5f);
+        RaycastHit2D hit = Physics2D.Raycast(hitPos1, Vector2.up,0.5f);
+        RaycastHit2D hit2 = Physics2D.Raycast(hitPos2, Vector2.up,0.5f);
 
-		return hit || hit2;
-	}
+        return hit || hit2;
+    }
 	
-	private void ResetCollisions()
-	{
-		_boxCollider2D.size = _standingColliderSize;
-		_boxCollider2D.offset = _standingColliderOffset;
-	}
+    private void ResetCollisions()
+    {
+        _boxCollider2D.size = _standingColliderSize;
+        _boxCollider2D.offset = _standingColliderOffset;
+    }
 
-	/// <summary>
-	/// Modifies speed by a ratio. If it's lower, it will return to normal after _timeOfSlowdown seconds, if it's higher it won't change until it is manually changed.
-	/// </summary>
-	public void ModifySpeed(float ratio)
-	{
-		_forwardSpeed = _maxForwardSpeed * 0.5f;
-	}
+    /// <summary>
+    /// Modifies speed by a ratio. If it's lower, it will return to normal after _timeOfSlowdown seconds, if it's higher it won't change until it is manually changed.
+    /// </summary>
+    public void ModifySpeed(float ratio)
+    {
+        _forwardSpeed = _maxForwardSpeed * 0.5f;
+        if (ratio < 1)
+        {
+            _isHit = true;
+        }
+    }
 }
