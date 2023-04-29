219f6041c597a6f79952cfc86c42819d9f6af048
Droodic <jeffcouture96@hotmail.com>
2023-04-29 01:33:54 -0400

    Revert "Auto stash before merge of "main" and "origin/main""
    This reverts commit 4b51e92e7d85cc180957ac6afafc19913cf15b15.

--- Assets/Scripts/Environment/EnvironmentAsset.cs
+++ Assets/Scripts/Environment/EnvironmentAsset.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 //Slice of the environment
 public class EnvironmentAsset : MonoBehaviour
 {
-<<<<<<< Updated upstream
 	//split the sprite up into positions that can be selected randomly to spawn obstacles at
 	private Transform[] obstaclePositions;
 
@@ -46,42 +45,3 @@ public class EnvironmentAsset : MonoBehaviour
 
 	}
 }
-=======
-    //split the sprite up into positions that can be selected randomly to spawn obstacles at
-    private Transform[] obstaclePositions;
-
-    private SpriteRenderer spriteRenderer;
-
-    private ProceduralEnvGenerator envGenerator;
-
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        envGenerator = ProceduralEnvGenerator.Get();
-    }
-
-    public float GetEnvironmentLength()
-    {
-        Sprite sprite = GetEnvironmentSprite();
-        return sprite.textureRect.width / sprite.pixelsPerUnit;
-    }
-
-    public Sprite GetEnvironmentSprite()
-    {
-        return spriteRenderer.sprite;
-    }
-
-    void Update()
-    {
-        // Check if any tiles have moved off the left side of the screen
-        float screenLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x - 10;
-
-        if (transform.position.x < screenLeft)
-        {
-            envGenerator.IncrementExpired();
-            Destroy(gameObject);
-        }
-
-    }
-}
->>>>>>> Stashed changes
