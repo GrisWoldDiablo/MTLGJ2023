f71f9eac10fe9795db8751f7360d00c23b64c174
Alonso <ugoalonso@hotmail.com>
2023-04-28 20:55:02 -0400

    Added Player prefab and ground tag

--- Assets/Alonso/Scripts/PlayerMovement.cs
+++ Assets/Alonso/Scripts/PlayerMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Types;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
-
+    private Rigidbody2D _body;
+    private BoxCollider2D _collider; 
     [SerializeField] private float _speed = 7f;
-
     [SerializeField] private float _jumpHeight = 14f;
+    [SerializeField] private ContactFilter2D _contactFilter2D;
+    private bool _isGrounded => _body.IsTouching(_contactFilter2D);
+
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        _body = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,10 +31,11 @@ public class PlayerMovement : MonoBehaviour
             dirX = 1f;
         }
 
-        body.velocity = new Vector2(dirX * _speed, body.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        _body.velocity = new Vector2(dirX * _speed, _body.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            body.velocity = new Vector3(body.velocity.x, _jumpHeight, 0);
+            _body.velocity = new Vector3(_body.velocity.x, _jumpHeight, 0);
         }
     }
+
 }
