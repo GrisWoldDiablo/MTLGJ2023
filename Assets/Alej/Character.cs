876cddc0d1cf36671c6154679c9397fc431160f9
GrisWoldDiablo <griswoldd@hotmail.com>
2023-04-29 12:52:18 -0400

    Adde invincible field

--- Assets/Alej/Character.cs
+++ Assets/Alej/Character.cs
@@ -3,28 +3,47 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
 	[SerializeField] private int _health;
+	[SerializeField] private bool _isInvincible = false;
 
 	private bool isDead = false;
 
-	public int Health { get => _health; set => _health = value; }
-    public bool IsDead { get => isDead; }
-
-	public PlayerMovement PlayerMovement { get => GetComponent<PlayerMovement>(); }
-
-    void Die()
+	public int Health
 	{
-        //game manager end game
-        isDead = true;
-    }
+		get => _health;
+		set => _health = value;
+	}
+
+	public bool IsDead
+	{
+		get => isDead;
+	}
+
+
+	public PlayerMovement PlayerMovement
+	{
+		get => GetComponent<PlayerMovement>();
+	}
+
+	void Die()
+	{
+		//game manager end game
+		isDead = true;
+	}
 
 	public void ModifyHealth(int damage)
 	{
+		if (_isInvincible)
+		{
+			return;
+		}
+
 		_health += damage;
 		if (_health <= 0)
 		{
 			Die();
 		}
 	}
+
 
 	public void ModifySpeed(float modification)
 	{
