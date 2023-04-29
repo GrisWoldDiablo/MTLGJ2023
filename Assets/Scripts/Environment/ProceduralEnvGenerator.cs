05d6458e3c243417cac7f0ce5941d71cb6d1b01f
Droodic <jeffcouture96@hotmail.com>
2023-04-29 19:46:49 -0400

    add concept of biomes, (volcano/cave/starnight) , setup volcano biome

--- Assets/Scripts/Environment/ProceduralEnvGenerator.cs
+++ Assets/Scripts/Environment/ProceduralEnvGenerator.cs
@@ -1,17 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.VersionControl;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UIElements;
+using UnityEngine.U2D;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+using System.Linq;
+
+public enum EBiomeType
+{
+	VolcanoArea = 0,
+	Cave		= 1,
+	NightSky	= 2
+};
+
+[System.Serializable]
+public class Biome
+{
+	[SerializeField] EBiomeType biomeType;
+    [SerializeField] private EnvironmentAsset[] environmentObjectList;
+    [SerializeField] private GameObject[] obstacleObjectList;
+
+    public EBiomeType BiomeType { get => biomeType; set => biomeType = value; }
+    public EnvironmentAsset[] EnvironmentObjectList { get => environmentObjectList; set => environmentObjectList = value; }
+    public GameObject[] ObstacleObjectList { get => obstacleObjectList; set => obstacleObjectList = value; }
+}
 
 public class ProceduralEnvGenerator : MonoBehaviour
 {
     private static ProceduralEnvGenerator _sInstance;
 
-    [Header("EnvironmentLists")]
-    [SerializeField] private EnvironmentAsset[] environmentObjectList;
-	[SerializeField] private GameObject[] obstacleObjectList;
+	[Header("EnvironmentLists")]
+	[SerializeField] List<Biome> biomeList = new List<Biome>();
 	[SerializeField] private int numAssetsToSpawnOnLoad = 10;
 	[SerializeField] private int numSlicesToSpawnOnRenew = 5;
 
-	//keeps track of the X coordinate at which to spawn the next environment slice
-	private float currentSpawnPostitionX = 0.0f;
+	private Biome currentBiome;
+
+    //keeps track of the X coordinate at which to spawn the next environment slice
+    private float currentSpawnPostitionX = 0.0f;
 	private float spawnPositionY = 0.0f;
 
 	//these should be the same
@@ -33,6 +64,18 @@ public class ProceduralEnvGenerator : MonoBehaviour
     public int NumSegmentsSinceLastObstacles { get => numSegmentsSinceLastObstacles; set => numSegmentsSinceLastObstacles = value; }
     public float RandomizedNumSegmentsBetweenObstacles { get => randomizedNumSegmentsBetweenObstacles;}
 
+	//cycle through available biomes and find the one that matches our set biome
+	private Biome GetCurrentBiome()
+	{
+		return biomeList[0];
+    }
+
+	private void AdvanceToNextBiome()
+	{
+        biomeList.Remove(currentBiome);
+		currentBiome = GetCurrentBiome();
+    }
+
     public static ProceduralEnvGenerator Get()
 	{
 		return _sInstance;
@@ -49,13 +92,13 @@ public class ProceduralEnvGenerator : MonoBehaviour
 
 	private EnvironmentAsset GetRandomEnvironmentAsset()
 	{
-		int randomAssetIndex = Random.Range(0, environmentObjectList.Length);
-		return environmentObjectList[randomAssetIndex];
+		int randomAssetIndex = Random.Range(0, currentBiome.EnvironmentObjectList.Length);
+		return currentBiome.EnvironmentObjectList[randomAssetIndex];
 	}
 	public GameObject GetRandomObstacle()
 	{
-        int randomAssetIndex = Random.Range(0, obstacleObjectList.Length);
-        return obstacleObjectList[randomAssetIndex];
+        int randomAssetIndex = Random.Range(0, currentBiome.ObstacleObjectList.Length);
+        return currentBiome.ObstacleObjectList[randomAssetIndex];
     }
 
 	private void RandomizeNumberSegmentsBetweenObstacles()
@@ -84,7 +127,6 @@ public class ProceduralEnvGenerator : MonoBehaviour
 		currentSpawnPostitionX += spacing;
     }
 
-
     public void IncrementExpired()
 	{
 		expiredSlicesCount++;
@@ -110,8 +152,9 @@ public class ProceduralEnvGenerator : MonoBehaviour
 	// Start is called before the first frame update
 	private void Start()
 	{
+		currentBiome = GetCurrentBiome();
 
-		RandomizeNumberSegmentsBetweenObstacles();
+        RandomizeNumberSegmentsBetweenObstacles();
 
 		//set first spawn position to leftmost coordinate of viewpoint
 		currentSpawnPostitionX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
