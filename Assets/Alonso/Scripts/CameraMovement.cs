711b59af880dffd56f06681bbd8109c6a705e783
Alonso <ugoalonso@hotmail.com>
2023-04-28 23:03:04 -0400

    Added way for player to run back with camera

--- Assets/Alonso/Scripts/CameraMovement.cs
+++ Assets/Alonso/Scripts/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,63 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Vector2 _offset = new (2.0f,2.3f);
+    private bool _allowCameraMovement = true;
+    private Camera _camera; //be sure to assign this in the inspector to your main camera
+    private Vector2 screenBounds;
+    private float objectWidth;
+
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = player.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
+
     void Update()
     {
-        transform.position = new Vector3(player.position.x + _offset.x, player.position.y + _offset.y,transform.position.z);
+        if (_allowCameraMovement)
+        {
+            transform.position = new Vector3(player.position.x + _offset.x, player.position.y + _offset.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y + _offset.y, transform.position.z);
+
+        }
     }
+    
+    void LateUpdate(){
+        if (!_allowCameraMovement)
+        {
+            //Clamp player movement when camera is not moving
+            screenBounds = _camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, _camera.transform.position.z));
+            
+            Vector3 viewPos = player.position;
+            viewPos.x = Mathf.Clamp(viewPos.x, transform.position.x - (screenBounds.x- transform.position.x) + objectWidth, screenBounds.x - objectWidth);
+            player.position = viewPos;
+        }
+    }
+
+    private Vector3 tempPlayerPos;
+    public void AllowCameraMovement(bool val)
+    {
+        if (!val && _allowCameraMovement)
+        {
+            tempPlayerPos = player.position;
+            _allowCameraMovement = false;
+        }
+
+        if (val && !_allowCameraMovement)
+        {
+            if(player.position.x > tempPlayerPos.x)
+            {
+                tempPlayerPos.y = player.position.y;
+                player.position = tempPlayerPos;
+                _allowCameraMovement = true;
+            }
+        }
+
+    }
+
 }
--- Assets/Alonso/Scripts/PlayerMovement.cs
+++ Assets/Alonso/Scripts/PlayerMovement.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private bool _leftAllowed = false;
+
     private Rigidbody2D _body;
+    [Header("Movement")]
     [SerializeField] private float _speed = 7f;
     [SerializeField] private float _jumpHeight = 14f;
+    
+    [Header("Camera")]
     [SerializeField] private CameraMovement _camera; 
+    
+    [Header("Collisions")]
     [SerializeField] private ContactFilter2D _contactFilter2D;
     private bool _isGrounded => _body.IsTouching(_contactFilter2D);
 
 	public float Speed { get => _speed; set => _speed = value; }
-
-	[SerializeField] private bool _leftAllowed = false;
   
     void Start()
     {
@@ -28,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
             if (_leftAllowed)
             {
                 dirX = -0.3f;
+                _camera.AllowCameraMovement(false);
             }
             else
             {
@@ -37,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
         }else if (dirX >= 0f)
         {
             dirX = 1.1f;
+            _camera.AllowCameraMovement(true);
         }
 
         _body.velocity = new Vector2(dirX * _speed, _body.velocity.y);
