18f799b7eb0f7d8bc9d8e79a432e4be0f29a68e6
Alonso <ugoalonso@hotmail.com>
2023-04-29 20:17:23 -0400

    Added collisions on HouseMaker

--- Assets/HouseGeneration.cs
+++ Assets/HouseGeneration.cs
@@ -5,24 +5,18 @@ using UnityEngine.Serialization;
 
 public class HouseGeneration : MonoBehaviour
 {
-   [SerializeField] private GameObject _topPos;
    [SerializeField] private GameObject _bottomPos;
 
    [SerializeField] private SpriteRenderer _partTemplate;
-   
-   [SerializeField] private Sprite[] _possibleTopParts;
-   [FormerlySerializedAs("_possibleParts")]
+
    [SerializeField] private Sprite[] _possibleBottomParts;
    
-   private Sprite[] _houseTopParts;
    private Sprite[] _houseBotParts;
    public int test = 0;
    void Start()
    {
       int i = test * 2;
-      _topPos.transform.localPosition = new Vector2(0, 6.5f);
      InitializeBottom(i);
-     InitializeTop(i);
    }
 
    private void InitializeBottom(int i)
@@ -47,35 +41,6 @@ public class HouseGeneration : MonoBehaviour
       {
          piece = Instantiate(_partTemplate, _bottomPos.transform);
          piece.sprite = _houseBotParts[k];
-         var tempX = piece.sprite.bounds.size.x ;
-         relativePos.x += tempX/2;
-
-         piece.gameObject.transform.localPosition = new Vector2(relativePos.x, piece.gameObject.transform.localPosition.y);
-         relativePos.x += tempX/2;
-      }
-   }
-   private void InitializeTop(int i)
-   {
-      _houseTopParts = new Sprite[i + 3];
-      _houseTopParts[0] = _possibleTopParts[0];
-      _houseTopParts[i/2 + 1] = _possibleTopParts[2];
-      _houseTopParts[i + 2] = _possibleTopParts[4];
-
-      for (int j = 0; j < i / 2; j++)
-      {
-         _houseTopParts[j + 1] = _possibleTopParts[1];
-         _houseTopParts[i + 1 - j] = _possibleTopParts[3];
-      }
-      Vector2 relativePos = new Vector2(0, 0);
-      var piece = Instantiate(_partTemplate, _topPos.transform);
-      piece.sprite = _houseTopParts[0];
-      piece.gameObject.transform.localPosition = new Vector2(relativePos.x, piece.gameObject.transform.localPosition.y);
-      relativePos.x += piece.sprite.bounds.size.x/2;
-
-      for (int k = 1; k < _houseTopParts.Length; k++)
-      {
-         piece = Instantiate(_partTemplate, _topPos.transform);
-         piece.sprite = _houseTopParts[k];
          var tempX = piece.sprite.bounds.size.x ;
          relativePos.x += tempX/2;
 
